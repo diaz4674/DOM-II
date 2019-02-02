1765de47879490b57bbb6ccca6e998da13c67019
Miguel <diaz4674@gmail.com>
2019-02-02 11:50:03 -0800

    var to let

--- js/index.js
+++ js/index.js
@@ -27,10 +27,10 @@ topHeader.appendChild(arrowParty);
 
 //Color Randomizer
 function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
  console.log(bgColor);
     document.body.style.background = bgColor;
     }
